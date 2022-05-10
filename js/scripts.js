c14bd3fcda48ed758cbc62487724ca14614a9c6a
Bukisani <103451235+Bukisani@users.noreply.github.com>
2022-05-10 12:49:47 -0500

    Update scripts.js
    updated the comments

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ function newItem() {
 
   crossOutButton.on('click', deleteListItem);
 
-  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css
+  //4. Adding CLASS DELETE (DISPLAY: NONE) from the css
   function deleteListItem() {
     $(this).closest('li').remove();
   }
