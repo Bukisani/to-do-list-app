27f089a3320438c65050808c011f6f6d3c2b81a1
Bukisani <103451235+Bukisani@users.noreply.github.com>
2022-05-11 13:34:13 -0500

    Create scripts.js
    moved the script to src folder

---
+++ src/scripts.js
@@ -0,0 +1,38 @@
+function newItem() {
+  //1. Adding a new item to the list:
+  let li = $('<li></li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('You must write something!');
+  } else {
+    $('#list').append(li);
+  }
+
+  //2.function that crosses out  an item out:
+  function crossOut() {
+    li.toggleClass('strike');
+  }
+  //2ii  crossing out an item after double clicking
+  li.on('dblclick', crossOut);
+
+  //3. Adding a delete button
+  let crossOutButton = $('<crossOutButton></crossOutButton>');
+  crossOutButton.append('X');
+  li.append(crossOutButton);
+
+  crossOutButton.on('click', deleteListItem);
+
+  //4. deletes item
+  function deleteListItem() {
+    $(this).closest('li').remove();
+  }
+
+  // 4. Reordering the items:
+  $('#list').sortable();
+}
+// avoiding the flickering after clicking the button
+$('#button').on('click', function (event) {
+  event.preventDefault();
+});
